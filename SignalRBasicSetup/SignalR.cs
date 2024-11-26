645fb8c4a3272637cab8f0f2eedc7eb0541772b1
rkishore1207 <rkishoreatr1207@gmail.com>
2024-11-26 17:43:27 +0530

    Created basic client-server setup for real time update

---
+++ SignalRBasicSetup/SignalR.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRBasicSetup
+{
+    public class SignalR : Hub
+    {
+        public async Task<int> GetUpdatedCount(int count)
+        {
+            await Clients.All.SendAsync("updateCount", count);
+            return count;
+        }
+    }
+}
